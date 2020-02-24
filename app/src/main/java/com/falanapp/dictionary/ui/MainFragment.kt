f854e7dfe48d93956611eff2431eb5e8764e8cc2
Yusuf Yiğit Yalmancı <yusuf.yalmanci@turkcell.com.tr>
2020-02-24 23:15:39 +0300

    Add fragments and toolbar

--- app/src/main/java/com/falanapp/dictionary/ui/MainFragment.kt
+++ app/src/main/java/com/falanapp/dictionary/ui/MainFragment.kt
@@ -2,21 +2,54 @@ package com.falanapp.dictionary.ui
 
 import android.os.Bundle
 import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import com.falanapp.dictionary.R
 
-class MainFragment :Fragment(){
 
+class MainFragment : Fragment() {
+    lateinit var searchView: SearchView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
 
+        val toolbar: androidx.appcompat.widget.Toolbar = view.findViewById(R.id.toolbar)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        setHasOptionsMenu(true)
+
+        searchView = view.findViewById(R.id.search_view)
+
+        searchView.setOnClickListener {
+            searchView.isIconified = false
+        }
+
+        return view
+    }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_main, menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val id = item.itemId
+
+        if (id == R.id.action_settings) {
+            return true
+        }
+
+        if (id == R.id.action_exit) {
+            System.exit(0)
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
